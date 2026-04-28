d16f14c7b4788172a4439bc2a33f188668b30308
Asendar1 <hamzahtaimour@gmail.com>
2026-04-28 17:42:46 +0300

    Fixed white and black init sprites, now they appear correctly (white at bottom)

--- include/piece.hpp
+++ include/piece.hpp
@@ -3,73 +3,107 @@
 
 class Piece
 {
-	protected:
-		sf::Sprite	image;
-		bool		isWhite;
+protected:
+	sf::Sprite image;
+	bool isWhite;
 
-	public:
-		Piece(bool isWhite) : isWhite(isWhite) {}
-		virtual ~Piece() = default;
-		void drawPiece(float x, float y, sf::RenderWindow &window);
+public:
+	Piece(bool isWhite) : isWhite(isWhite) {}
+	virtual ~Piece() = default;
+	void virtual drawPiece(float x, float y, sf::RenderWindow &window);
 };
 
 class Pawn : public Piece
 {
-	private:
-		bool hasMoved;
-		sf::Texture tex;
+private:
+	bool hasMoved;
+	sf::Texture tex;
 
-	public:
-		Pawn(bool isWhite) : Piece(isWhite), hasMoved(false)
+public:
+	Pawn(bool isWhite) : Piece(isWhite), hasMoved(false)
+	{
+
+		if (!isWhite)
 		{
-
-			if(!tex.loadFromFile("sprites/black_pawn.png"))
+			if (!tex.loadFromFile("sprites/black_pawn.png"))
 			{
 				std::cerr << "Failed to load pawn image" << std::endl;
 			}
-			if (!isWhite)
-				image.setTexture(tex);
+			image.setTexture(tex);
 		}
-		~Pawn() = default;
+		else
+		{
+			if (!tex.loadFromFile("sprites/white_pawn.png"))
+			{
+				std::cerr << "Failed to load pawn image" << std::endl;
+			}
+			image.setTexture(tex);
+		}
+	}
+	~Pawn() = default;
+	void virtual drawPiece(float x, float y, sf::RenderWindow &window) override;
 };
 
 class Rook : public Piece
 {
-	private:
-		bool hasMoved;
+private:
+	bool hasMoved;
 
-	public:
-		Rook(bool isWhite) : Piece(isWhite), hasMoved(false) {}
-		~Rook() = default;
+public:
+	Rook(bool isWhite) : Piece(isWhite), hasMoved(false) {}
+	~Rook() = default;
 };
 
 class Knight : public Piece
 {
-	public:
-		Knight(bool isWhite) : Piece(isWhite) {}
-		~Knight() = default;
+private:
+	sf::Texture tex;
+
+public:
+	Knight(bool isWhite) : Piece(isWhite)
+	{
+		if (!isWhite)
+		{
+			if (!tex.loadFromFile("sprites/black_knight.png"))
+			{
+				std::cerr << "Failed to load knight image" << std::endl;
+			}
+			image.setTexture(tex);
+		}
+		else
+		{
+			if (!tex.loadFromFile("sprites/white_knight.png"))
+			{
+				std::cerr << "Failed to load knight image" << std::endl;
+			}
+			image.setTexture(tex);
+		}
+	}
+	~Knight() = default;
+
+	void virtual drawPiece(float x, float y, sf::RenderWindow &window) override;
 };
 
 class Bishop : public Piece
 {
-	public:
-		Bishop(bool isWhite) : Piece(isWhite) {}
-		~Bishop() = default;
+public:
+	Bishop(bool isWhite) : Piece(isWhite) {}
+	~Bishop() = default;
 };
 
 class Queen : public Piece
 {
-	public:
-		Queen(bool isWhite) : Piece(isWhite) {}
-		~Queen() = default;
+public:
+	Queen(bool isWhite) : Piece(isWhite) {}
+	~Queen() = default;
 };
 
 class King : public Piece
 {
-	private:
-		bool hasMoved;
+private:
+	bool hasMoved;
 
-	public:
-		King(bool isWhite) : Piece(isWhite), hasMoved(false) {}
-		~King() = default;
+public:
+	King(bool isWhite) : Piece(isWhite), hasMoved(false) {}
+	~King() = default;
 };
--- srcs/piece.cpp
+++ srcs/piece.cpp
@@ -1,10 +1,17 @@
 #include "piece.hpp"
 
-void Piece::drawPiece(float x, float y, sf::RenderWindow &window)
+void Piece::drawPiece(float , float , sf::RenderWindow &)
 {
-	if (dynamic_cast<Pawn *>(this))
-	{
-		image.setPosition(sf::Vector2f(x * 120 + 24, y * 120 + 12));
-		window.draw(image);
-	}
+}
+
+void Pawn::drawPiece(float x, float y, sf::RenderWindow &window)
+{
+	image.setPosition(sf::Vector2f(x * 120 + 24, y * 120 + 12));
+	window.draw(image);
+}
+
+void Knight::drawPiece(float x, float y, sf::RenderWindow &window)
+{
+	image.setPosition(sf::Vector2f(x * 120 + 24, y * 120 + 12));
+	window.draw(image);
 }
